14c3dc313f2009547d6ea30512787a3513f9c412
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-02 14:02:17 +0000

    submitted answer to q1

--- extract_variable/1.rb
+++ extract_variable/1.rb
@@ -1,3 +1,15 @@
-def send_email_to(user)
-  Mail.new(user.email.strip).send_message
+# def send_email_to(user)
+#   Mail.new(user.email.strip).send_message
+# end
+
+class Mail
+  def initialize(addressee, subject = nil, body = nil)
+    @addressee = addressee
+    @subject = subject
+    @body = body
+  end
+
+  def send_message
+    # Sends message to @addressee
+  end
 end
