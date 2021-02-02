aed6d38bc54a13a856211758fcba9b5504ddd165
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-02 14:12:38 +0000

    submitted answer to q2

--- extract_variable/2.rb
+++ extract_variable/2.rb
@@ -1,7 +1,23 @@
+# def tell_browser_type(browser)
+#   if(browser.type == "Safari")
+#     puts "You are using the Safari browser."
+#   else
+#     puts "You are using a non-Safari browser."
+#   end
+# end
+
+class Browser
+  def initialize(type = 'Safari')
+    @type = type
+  end
+end
+
 def tell_browser_type(browser)
-  if(browser.type == "Safari")
-    puts "You are using the Safari browser."
+  desired = 'Safari'
+  
+  if browser.type == desired
+    puts "You are using the #{desired} browser"
   else
-    puts "You are using a non-Safari browser."
+    puts "You are using the non-#{desired} browser"
   end
 end
