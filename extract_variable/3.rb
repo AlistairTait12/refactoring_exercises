a281d01f679e695453babef86c6c12e880c5c3fa
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-03 13:50:45 +0000

    extracted variables from q3

--- extract_variable/3.rb
+++ extract_variable/3.rb
@@ -5,3 +5,14 @@ def send_correct_page(browser, window)
     return "You are not using the Safari browser or have a big window."
   end
 end
+
+def send_correct_page(browser, window)
+  small = "768px"
+  desired_browser = "Safari"
+
+  if (browser.type == desired_browser && window.size < small
+    return "You are using the #{desired_browser} browser in a small window"
+  else
+    return "You are not using the #{desired_browser} or have a big window"
+  end
+end
